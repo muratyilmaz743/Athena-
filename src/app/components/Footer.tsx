7efd7db331f9548a1096068cd9bab3b830ea64a4
Murat <mrt.yilmaz743@gmail.com>
2023-08-13 20:17:32 +0300

    footer upgraded

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,20 +4,20 @@ import { Container, Typography, Link, makeStyles, styled } from "@mui/material";
 
 function Footer() {
   const CustomTypography = styled(Typography)({
-    color:'white'
+    color: 'white'
   });
 
   return (
-    <Container maxWidth="xl" disableGutters style={{ marginTop: '2rem', paddingBottom: '2rem', paddingTop:'2rem', background: '#1976d2' }}>
+    <Container maxWidth="xl" disableGutters style={{ marginTop: '2rem', paddingBottom: '2rem', paddingTop: '2rem', background: '#1976d2' }}>
       <CustomTypography variant="body2" color="textSecondary" align="center">
-        Made with ❤️ by Felece UI Team
+        Made with ❤️ by Murat YILMAZ
       </CustomTypography>
       <CustomTypography variant="body2" color="textSecondary" align="center">
-        Copyright &copy; 2023
+        <Link variant="body2" color="textPrimary" align="center" href="https://github.com/muratyilmaz743">
+          My GITHUB
+        </Link>
       </CustomTypography>
-      <CustomTypography variant="body2" color="textSecondary" align="center">
-        <Link color="inherit" href="#">Terms of Use</Link> | <Link color="inherit" href="#">Privacy Policy</Link>
-      </CustomTypography>
+
     </Container>
   );
 }
